165aac9412c058047202f9fbe02f3320f070d2b4
BLyman <135188980+Lyman17@users.noreply.github.com>
2023-08-13 14:51:22 -0600

    meta & Script API key

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const API_KEY = "4238b6088b28bd7b216e5c6759811817"
+
+//calls the fetchWeatherAPI function and clears the search input
+function search(){
+    var searchInput = $("#search-input").val();
+    fetchWeatherAPI(searchInput);
+    $("#search-input").val("");
+}
