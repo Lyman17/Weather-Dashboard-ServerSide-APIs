67e4aa77773d6316531212d7eed285ca21f7c142
BLyman <135188980+Lyman17@users.noreply.github.com>
2023-08-13 16:32:21 -0600

    fetch funtions js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,51 @@ function search(){
     $("#search-input").val("");
 }
 
+//crates a fetch for weather, returning alerts if there are errors, returning API data if status ok, calls functions to display data from API
+function fetchWeatherAPI(input){
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + API_KEY +"&units=imperial";
+    fetch(queryURL)
+        .then(function(response){
+            if(response.ok){
+                return response.json();
+            }else if(response.status == 400){
+                alert("HTTP-Error: " + response.status + "\nPlease enter a valid city name in the Search bar.");
+            }else if(response.status == 404){
+                alert("HTTP-Error: " + response.status + "\nThe city name you entered is not valid.\nPlease enter a valid city name in the Search bar.");
+            }else{
+                alert("HTTP-Error: " + response.status);
+            }
+            
+        })
+        .then(function(data){
+            var name = data.name;
+            var testKey = "HDF-weatherApp-" + name;
+            if(!(localStorage.getItem(testKey))){
+                addToList(name);
+            }
+            fetchForecastAPI(name);
+            displayToday(data);
+            
+        });
+}
+//creates a fetch for 5day forecast, returns alerts if there are errors, returning API data if status ok, calls functions to display 5-day forecast
+function fetchForecastAPI(input){
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=" + API_KEY +"&units=imperial&";
+    fetch(queryURL)
+        .then(function(response){
+            if(response.ok){
+                return response.json();
+            }else if(response.status == 400){
+                alert("HTTP-Error: " + response.status + "\nPlease enter a valid city name in the Search bar.");
+            }else if(response.status == 404){
+                alert("HTTP-Error: " + response.status + "\nThe city name you entered is not valid.\nPlease enter a valid city name in the Search bar.");
+            }else{
+                alert("HTTP-Error: " + response.status);
+            }
+            
+        })
+        .then(function(data){
+            display_5day(data);
+        });
+    
+}
